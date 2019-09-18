29763fedeae68a8aca3ef77b91de5530ca34c87b
Benjamin55 <bajramovicbenjamin91@gmail.com>
2019-09-18 14:22:54 +0200

    new portfolio

---
+++ index.php
@@ -0,0 +1,363 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css" />
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="php" href="/contactform.php">
+  <title>Benjamin Bajramović</title>
+</head>
+<body>
+    <!-- header -->
+    <body>
+            <div class="container">
+              <header id="main-header">
+                <div class="row no-gutters">
+                  <div class="col-lg-4 col-md-5">
+                    <img src="img/be.jpg" alt="">
+                  </div>
+                  <div class="col-lg-8 col-md-7">
+                    <div class="d-flex flex-column">
+                      <div class="p-5 bg-dark text-white">
+                        <div class="d-flex flex-row justify-content-between align-items-center">
+                          <h1 class="display-4">Benjamin Bajramović</h1>
+                          
+                          <div class="d-none d-md-block">
+                            <a href="https://www.facebook.com/Ra1Ndown?ref=bookmarks" class="text-white">
+                              <i class="fab fa-facebook"></i>
+                            </a>
+                          </div>
+                          <div class="d-none d-lg-block">
+                            <a href="https://www.instagram.com/ra1n_official/?hl=hr" class="text-white">
+                              <i class="fab fa-instagram"></i>
+                            </a>
+                          </div>
+                          <div class="d-none d-lg-block">
+                            <a href="https://www.youtube.com/channel/UCClU428TBnKtoVn9gEQSiZw?view_as=subscriber" class="text-white">
+                              <i class="fab fa-youtube"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+          
+                      <div class="p-4 bg-black">
+                        Bachelor of Information Technology
+                      </div>
+          
+                      <div>
+                        <div class="d-flex flex-row text-white align-items-stretch text-center">
+                          <div class="port-item p-4 bg-primary" data-toggle="collapse" data-target="#home">
+                            <i class="fas fa-home fa-2x d-block"></i>
+                            <span class="d-none d-sm-block">Home</span>
+                          </div>
+                          <div class="port-item p-4 bg-success" data-toggle="collapse" data-target="#resume">
+                            <i class="fas fa-graduation-cap fa-2x d-block"></i>
+                            <span class="d-none d-sm-block">Resume</span>
+                          </div>
+                          <div class="port-item p-4 bg-warning" data-toggle="collapse" data-target="#work">
+                            <i class="fas fa-folder-open fa-2x d-block"></i>
+                            <span class="d-none d-sm-block">Work</span>
+                          </div>
+                          <div class="port-item p-4 bg-danger" data-toggle="collapse" data-target="#contact">
+                            <i class="fas fa-envelope fa-2x d-block"></i>
+                            <span class="d-none d-sm-block">Contact</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </header>
+
+<!-- home -->
+<div id="home" class="collapse show">
+    <div class="card card-body bg-primary text-white py-5">
+        <h2>Welcome To My Page</h2>
+        <p class="lead">You can find out something more about me.</p>
+    </div>
+
+    <div class="card card-body py5">
+        <h3>My Skills</h3>
+        <p>Here are some of my professional skills.</p>
+        <hr>
+        <h4>HTML 5</h4>
+        <div class="progress mb-3">
+            <div class="progress-bar bg-success" style="width:100%"></div>
+        </div>
+        <h4>CSS 3</h4>
+        <div class="progress mb-3">
+            <div class="progress-bar bg-success" style="width:100%"></div>
+        </div>
+        <h4>Bootstrap 4</h4>
+        <div class="progress mb-3">
+            <div class="progress-bar bg-success" style="width:60%"></div>
+        </div>
+        <h4>JavaScript</h4>
+        <div class="progress mb-3">
+            <div class="progress-bar bg-success" style="width:30%"></div>
+        </div>
+        <h4>Photoshop</h4>
+        <div class="progress mb-3">
+            <div class="progress-bar bg-success" style="width:30%"></div>
+        </div>
+        <h4>Shotcut</h4>
+        <div class="progress mb-3">
+            <div class="progress-bar bg-success" style="width:100%"></div>
+        </div>
+        <h4>Ableton live</h4>
+        <div class="progress mb-3">
+            <div class="progress-bar bg-success" style="width:100%"></div>
+        </div>
+        <h4>Ableton live</h4>
+        <div class="progress mb-3">
+            <div class="progress-bar bg-success" style="width:100%"></div>
+        </div>
+
+    </div>
+</div>
+
+
+
+<!-- resume -->
+<div id="resume" class="collapse">
+        <div class="card card-body bg-success text-white py-5">
+                <h2>My Resume</h2>
+                <p class="lead">You can find out something more about my work experience.</p>
+            </div>
+            <div class="car card-body py-5">
+                <h3>Where have i worked</h3>
+                <p>Here are my jobs and projects.</p>
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Township Zenica</h4>
+                            <p class="card-text">IT Sector.</p>
+                            <p class="p-2 mb-3 bg-dark text-white">Program and System Administration.</p>
+                            <p class="p-2 mb-3 bg-dark text-white">+387 32 447 700</p>
+                            <div class="text-muted">Dates: 2017-2018</div>
+                        </div>
+                        
+                    </div>
+                    <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Elevation</h4>
+                                <p class="card-text">Electronic music event organisation.</p>
+                                <p class="p-2 mb-3 bg-dark text-white">Event Manager, DJ.</p>
+                                <p class="p-2 mb-3 bg-dark text-white">+387 62 880 882</p>
+                                <div class="text-muted">Dates: 2017- </div>
+                            </div>
+                            
+                        </div>
+                        <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Trash challenge</h4>
+                                    <p class="card-text">Environment Conservation Organisation</p>
+                                    <p class="p-2 mb-3 bg-dark text-white">Team Leader.</p>
+                                    <p class="p-2 mb-3 bg-dark text-white">+387 61 624 582</p>
+                                    <div class="text-muted">Dates: 2018- </div>
+                                </div>
+                                
+                            </div>
+                    
+                    
+                </div>
+                
+            </div>
+         <hr>
+          <div class="car card-body py-5">
+              <h3>Certificates</h3>
+              <p></p>
+              <div class="card-deck">
+                  <div class="card">
+                      <div class="card-body">
+                          <h4 class="card-title">Web design</h4>
+                          <p class="card-text"></p>
+                          <p class="p-2 mb-3 bg-dark text-white">Certificate of web design</p>
+                          <p class="p-2 mb-3 bg-dark text-white">ZEDA, Zenica</p>
+                          <div class="text-muted">Date: 20.10.2017.</div>
+                      </div>
+                      
+                  </div>
+                  <div class="card">
+                          <div class="card-body">
+                              <h4 class="card-title">Business management</h4>
+                              <p class="card-text"></p>
+                              <p class="p-2 mb-3 bg-dark text-white">Youth employment project</p>
+                              <p class="p-2 mb-3 bg-dark text-white">YEP, Zenica</p>
+                              <div class="text-muted">Date: 19.6.2018.</div>
+                          </div>
+                          
+                      </div>
+                      <div class="card">
+                              <div class="card-body">
+                                  <h4 class="card-title">English language</h4>
+                                  <p class="card-text"></p>
+                                  <p class="p-2 mb-3 bg-dark text-white">B2, Proficient user</p>
+                                  <p class="p-2 mb-3 bg-dark text-white">Language Masters Agency, Zenica</p>
+                                  <div class="text-muted">Date: 25.1.2017.</div>
+                              </div>
+                              
+                          </div>
+                  
+                  
+              </div>
+              
+          </div>
+            
+</div>
+
+<!-- work -->
+<div id="work" class="collapse">
+        <div class="card card-body bg-warning text-white py-5">
+                <h2>My work</h2>
+                <p class="lead">Picture gallery of some of my projects.</p>
+            </div>
+
+            <div class="card card-body py-5">
+                <h3>What Have i Done</h3>
+                <p></p>
+                <div class="row ">
+                    <div class="col-md-4">
+                        <a href="img/1.jpg" data-toggle="lightbox">
+                        <img src="img/1.jpg" alt=""class="img-fluid" >
+                        </a>           
+                    </div>
+                    <div class="col-md-4">
+                            <a href="img/2.jpg" data-toggle="lightbox">
+                            <img src="img/2.jpg" alt="" class="img-fluid">
+                            </a>           
+                        </div>
+                        <div class="col-md-4">
+                                <a href="img/4.jpg" data-toggle="lightbox">
+                                <img src="img/4.jpg" alt="" class="img-fluid">
+                                </a>           
+                            </div>
+                </div>
+                <hr>
+                <div class="row">
+                        <div class="col-md-4">
+                            <a href="img/3.jpg" data-toggle="lightbox">
+                            <img src="img/3.jpg" alt=""class="img-fluid" >
+                            </a>           
+                        </div>
+                        <div class="col-md-4">
+                                <a href="img/5.jpg" data-toggle="lightbox">
+                                <img src="img/5.jpg" alt="" class="img-fluid">
+                                </a>           
+                            </div>
+                            <div class="col-md-4">
+                                    <a href="img/6.jpg" data-toggle="lightbox">
+                                    <img src="img/6.jpg" alt="" class="img-fluid">
+                                    </a>           
+                                </div>
+                    </div>
+                
+            </div>
+</div>
+
+<!-- contact -->
+
+<div id="contact" class="collapse">
+  <div class="card card-body bg-danger text-white py-5">
+    <h2>Contact</h2>
+    <p class="lead"></p>
+  </div>
+
+  <div class="card card-body py-5">
+    <h3>Get In Touch</h3>
+    <p></p>
+    <form class="contact-form" action="mailto:bajramovicbenjamin91@gmail.com" method="post" enctype="text/plain">
+      <div class="form-group">
+        <div class="input-group input-group-lg">
+          <div class="input-group-prepend">
+            <span class="input-group-text bg-danger text-white">
+              <i class="fas fa-user"></i>
+            </span>
+          </div>
+          <input type="text" class="form-control bg-dark text-white" placeholder="Your Name">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="input-group input-group-lg">
+          <div class="input-group-prepend">
+            <span class="input-group-text bg-danger text-white">
+              <i class="fas fa-envelope"></i>
+            </span>
+          </div>
+          <input type="email" class="form-control bg-dark text-white" placeholder="Your Email">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="input-group input-group-lg">
+          <div class="input-group-prepend">
+            <span class="input-group-text bg-danger text-white">
+              <i class="fas fa-pencil-alt"></i>
+            </span>
+          </div>
+          <textarea name="msg" class="form-control bg-dark text-white" placeholder="Your Message"></textarea>
+        </div>
+      </div>
+
+      <input type="submit" value="Submit" class="btn btn-danger btn-block btn-lg">
+    </form>
+  </div>
+</div>
+
+
+              <!-- footer -->
+              <footer id="main-footer" class="p-5 bg-dark text-white">
+                  <div class="row">
+                      <div class="col-md-6">
+                          <a href="benjamin.pdf" class="btn btn-outline-light">
+                              <i class="fas fa-cloud"></i> Download Resume
+                          </a>
+                      </div>
+                  </div>
+
+              </footer>
+          </div>
+             
+<!--  -->
+
+
+</body>
+
+
+
+
+
+
+
+
+?><script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
+<script src="ajax-contact.js"></script>
+<script>
+$('.port-item').click(function () {
+  $('.collapse').collapse('hide');
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function (e) {
+  e.preventDefault();
+  $(this).ekkoLightbox();
+});
+</script>
+</body>
+
+</html>
